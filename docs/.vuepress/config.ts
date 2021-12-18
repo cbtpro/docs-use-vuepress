5365c46e7e3a77251b8db5d843e56b0a9b6fde26
陈碧滔 <cbtpro@163.com>
2021-12-18 14:36:55 +0800

    修改打包脚本

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,4 +9,7 @@ export default defineUserConfig<DefaultThemeOptions>({
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
   },
+  // 在使用 vuepress-vite 包的时候，你可以忽略这个字段，因为 Vite 是默认打包工具
+  bundler: '@vuepress/bundler-vite',
+  dest: './dist',
 })
